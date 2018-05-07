a61d57071ce35b8e4b4ff20d2cdc7c4bb62eaf56
Giorgi Maisuradze <g.maisuradze88@gmail.com>
2018-05-07 23:30:10 +0400

    fix

--- z-route-initializer.js
+++ z-route-initializer.js
@@ -1,22 +1,27 @@
 'use strict';
 
 module.exports = {
-    run: function (app, next) {
-        var beforeRoute = app.config.zMiddleware.beforeRoute;
-        for (var i in beforeRoute.middleware) {
-            require(app.folderPath.app.root + beforeRoute.rootDir + '/' + beforeRoute.middleware[i])(app);
+    run: (app, next) => {
+        if (app.config.zMiddleware && pp.config.zMiddleware.beforeRoute) {
+            const beforeRoute = app.config.zMiddleware.beforeRoute;
+            for (let middleware of beforeRoute.middleware) {
+                require(app.folderPath.app.root + beforeRoute.rootDir + '/' + middleware)(app);
+            }
         }
 
-        var routes = app.config.zRoute.routes;
-        for (var i in routes) {
+        const routes = app.config.zRoute.routes;
+        for (let i in routes) {
             var module = require(app.folderPath.app.root + app.config.zRoute.rootDir + '/' + routes[i]);
             app.express.use(i, module(app));
         }
 
-        var afterRoute = app.config.zMiddleware.afterRoute;
-        for (var i in afterRoute.middleware) {
-            require(app.folderPath.app.root + afterRoute.rootDir + '/' + afterRoute.middleware[i])(app);
+        if (app.config.zMiddleware && app.config.zMiddleware.afterRoute) {
+            const afterRoute = app.config.zMiddleware.afterRoute;
+            for (let middleware of afterRoute.middleware) {
+                require(app.folderPath.app.root + afterRoute.rootDir + '/' + middleware)(app);
+            }
         }
+
         next();
     }
 };
